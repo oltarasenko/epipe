84bb1432e57b837e0eef632b1e334027d1477302
raydrawc <raydraw@163.com>
2018-09-17 16:16:02 +0800

    add more args

--- src/epipe.erl
+++ src/epipe.erl
@@ -13,15 +13,23 @@
       InitialState :: term(),
       PipeFun      :: fun((SomeState :: term()) -> PipeFunRes),
       PipeFunRes   :: {ok, NextState :: term()}
+                    | {stop, State :: term()}
                     | {error, Reason :: term()}
                     | {error, Reason :: term(), NewState :: term()},
       Result       :: {ok, State :: term()}
+                    | {ok, Label, State :: term()}
+                    | {stop, Label, State::term()}
                     | {error, Label, Reason :: term(), NewState :: term()}.
+
 run([], State) ->
     {ok, State};
+run(FuncList, State) when erlang:is_tuple(State) ->
+    run(FuncList, erlang:tuple_to_list(State));
 run([{Label, Func} | Rest], State) ->
-    case Func(State) of
+    case erlang:apply(Func, State) of
+        ok                        -> {ok, Label, State};
         {ok, NewState}            -> run(Rest, NewState);
+        {stop, NewState}          -> {stop, Label, NewState};
         {error, Reason}           -> {error, Label, Reason, State};
         {error, Reason, NewState} -> {error, Label, Reason, NewState}
     end.
